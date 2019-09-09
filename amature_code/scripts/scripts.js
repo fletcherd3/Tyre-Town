741747e5f7f50aa30de9792b39e206282ca60dfc
Fletcher Dick <fdi19@uclive.ac.nz>
2019-09-09 14:34:07 +1200

    Added error_handller func and make_collapsible func to reduce clutter in index.php

--- amature_code/scripts/scripts.js
+++ amature_code/scripts/scripts.js
@@ -1,1 +1,30 @@
 // Keep your scripts in here
+
+
+function error_handller() {
+    onerror = errorHandler;
+    function errorHandler(message, link, line) {
+        message = "Error Encountered:\n\n";
+        message += "URL: " + link + "\n";
+        message += "Line: " + line + "\n";
+        alert(message);
+        return true;
+    }
+}
+
+function make_collapsible() {
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i= 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+}
